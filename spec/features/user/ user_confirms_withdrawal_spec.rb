0180ef8f41578fea3a06767776bbab70dc400990
Lucas Dourado <lucashenrique.2d@gmail.com>
2019-08-27 19:46:03 -0300

    correcao de teste

--- spec/features/user/ user_confirms_withdrawal_spec.rb
+++ spec/features/user/ user_confirms_withdrawal_spec.rb
@@ -3,17 +3,17 @@ feature 'User confirm withdrawal order' do
   scenario 'successfully' do
     user = User.create(email: 'jo@gmail.com', password: '12345678')
 
-    RemovalOrder.create(weight: 10,
+    removal_order1 = RemovalOrder.create(weight: 10,
                         removal_date_start: '01/09/2000',
                         removal_date_end: '02/09/2000',
                         address: 'rua das bolinhas n 02',
                         user: user, status: :open)
 
-    RemovalOrder.create(weight: 19,
+    removal_order2 = RemovalOrder.create(weight: 19,
                         removal_date_start: '06/09/2000',
                         removal_date_end: '08/09/2000',
                         address: 'rua das bolinhas n 88',
-                        user: user, status: :close)
+                        user: user, status: :open)
 
     visit root_path
     click_on 'Logar como Usuario'
@@ -21,9 +21,13 @@ feature 'User confirm withdrawal order' do
     fill_in 'Senha', with: user.password
     click_on 'Logar'
     click_on 'Todos pedidos em aberto'
-    click_on 'Confirmar retirada'
+    within ".removal-order-#{removal_order1.id}" do
+      click_on('Confirmar retirada')
+    end
 
+    removal_order1.reload
+    expect(removal_order1.status).to eq 'close'
+    expect(removal_order2.status).to eq 'open'
     expect(page).to have_content 'Pedido encerrado com sucesso!'
-    expect(page).to have_content 'Nenhuma coleta disponivel'
   end
 end

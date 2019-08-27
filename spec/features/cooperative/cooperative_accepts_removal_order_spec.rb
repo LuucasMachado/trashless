89c1c7b31cade992dbf7ec5c1309f3236d71d286
Mateus Braga <mateuscbraga@gmail.com>
2019-08-27 12:02:56 -0300

    adjusted a few tests

--- spec/features/cooperative/cooperative_accepts_removal_order_spec.rb
+++ spec/features/cooperative/cooperative_accepts_removal_order_spec.rb
@@ -1,5 +1,4 @@
 require 'rails_helper'
-
 feature 'Cooperative accepts a removal order' do
   scenario 'successfully' do
     coop = create(:cooperative)
@@ -7,13 +6,13 @@ feature 'Cooperative accepts a removal order' do
     garbage_man = create(:garbage_man, cooperative: coop)
     removal_order = create(:removal_order, user: user, status: :open)
     
-    sign_in (coop)
+    sign_in(coop, :scope => :coop)
     visit "/removal_order/#{removal_order.id}"
-    select garbage_man.name, :from => "Coletor"
-    click_on "aceitar"
+    select garbage_man.name, from: 'Coletor'
+    click_on 'aceitar'
 
-    expect(page).to have_content("#{removal_order.order_title}")
+    expect(page).to have_content("##{removal_order.id}-#{removal_order.wight}")
     expect(page).to have_content("coletor: #{garbage_man.name}")
-    expect(page).to have_content("status: em andamento")
+    expect(page).to have_content('status: em andamento')
   end
 end
--- spec/models/removal_order_spec.rb
+++ spec/models/removal_order_spec.rb
@@ -1,4 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe RemovalOrder, type: :model do
+  describe '#order_title' do
+    it 'should return title in format $id-$weight' do
+      expect(true).to eql false
+    end
+  end
 end

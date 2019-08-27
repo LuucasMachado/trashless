1c549c518af1f0d129039dbfeea5f7d2f7a8de03
Mateus Braga <mateuscbraga@gmail.com>
2019-08-27 16:34:37 -0300

    l

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   devise_for :cooperatives
   devise_for :users
-  resources :removal_orders
+  resources :removal_orders do
+    member do
+      post :accept
+    end
+  end
   resources :garbage_men
   root to: "home#index"
 end

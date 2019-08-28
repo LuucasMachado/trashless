61c279db96041b83b8beaecd61b18f7593ccba55
Mateus Braga <mateuscbraga@gmail.com>
2019-08-28 13:29:49 -0300

    fazendo os tests passarem

--- app/models/removal_order.rb
+++ app/models/removal_order.rb
@@ -4,7 +4,7 @@ class RemovalOrder < ApplicationRecord
   validates :weight, :removal_date_start, :removal_date_end,
             :description, :address, presence: true
   belongs_to :garbage_man, optional: true
-  enum status: { open: 0, in_progress: 1, close: 5 }
+  enum status: { open: 0, in_progress: 1, in_problem: 2, close: 5 }
 
   validate :garbage_man_must_exist, on: :accept_removal_order
   def garbage_man_must_exist
